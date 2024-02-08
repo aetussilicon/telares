46575490e9d0ffdddaba2504a1df04113c918eb5
Artur Gomes <arturgaollidev@outlook.com>
2024-02-08 19:20:42 -0300

    Pagina de compra criada e cards da página

--- src/Components/Elements/ShopCard.jsx
+++ src/Components/Elements/ShopCard.jsx
@@ -2,12 +2,12 @@ import "../../Styles/css/ShopCard.css"
 
 function ShopCard() {
     return (
-        <div className="container">
+        <div className="container shop-card-container">
             <div className="shopCard">
                 <img className="itemIcon" src="backgrounds/backgroundShop.png" alt="icon" />
                 <div className="cardInfo">
                     <h2 className="cardTitle">Exemple Product</h2>
-                    <p className="stars">🟊🟊🟊🟊🟊</p>
+                    <p className="stars no-hover">🟊🟊🟊🟊🟊</p>
                     <p className="price">€39.00</p>
                     <button type="button" className="btn btn-secondary cardButton">Add to cart</button>
                 </div>
--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -1,41 +1,46 @@
 import Header from "../Elements/Header";
 import ShopCard from "../Elements/ShopCard";
 import '../../Styles/css/Shop.css'
+import PageFooter from "../Elements/PageFooter";
+import { Link } from "react-router-dom";
 
 function Shop() {
 
     let result = 12;
 
-
     return (
         <>
-            <Header />
-            <body>
-                <div className="container">
-                    <div className="banner bannerShop">
+            <div className="container">
+                <Header />
+                <div className="main-banner banner-shop">
+                    <div className="banner-shop-text">
+                        <h1>Lorem Ipsum</h1>
+                        <p className="no-hover">outdoor fabrics lonas para exterior</p>
                     </div>
-                    <div className="resultGrid d-flex justify-content-between">
-                        <div className="filtersColumn">
-                            <div className="filters">
-                                <h3>Caribe Proof</h3>
-                                <p>Rustik</p>
-                                <p>Deco</p>
-                                <p>Advance</p>
-                                <p>Tierra</p>
-                            </div>
-                            <div className="filters">
-                                <h3>Filtrar Por</h3>
-                                <h2>Precio</h2>
-                                <h2>Color</h2>
-                                <h2>Tipo de tela</h2>
-                            </div>
+                </div>
+                <div className="result-grid">
+                    <div className="filters-column">
+                        <div className="filters">
+                            <h3>Caribe Proof</h3>
+                            <p>Rustik</p>
+                            <p>Deco</p>
+                            <p>Advance</p>
+                            <p>Tierra</p>
                         </div>
-                        <div className="searchResult" >
-                            <div className="topRow d;-flex">
-                                <p>Showing all {result} results</p>
+                        <div className="filters">
+                            <h3>Filtrar Por</h3>
+                            <h2>Precio</h2>
+                            <h2>Color</h2>
+                            <h2>Tipo de tela</h2>
+                        </div>
+                    </div>
+                    <div className="search-result" >
+                        <div className="top-row">
+                            <p className="count-of-results no-hover">Showing all {result} results</p>
+                            <div className="dropdown-menus">
                                 <div className="dropdown">
-                                    <button className="dropdownButton d-flex">
-                                        <p>Default sorting</p>
+                                    <button className="dropdown-button">
+                                        <p className="no-hover">Default sorting</p>
                                         <p>▼</p>
                                     </button>
                                     <div className="dropdown-content">
@@ -46,49 +51,43 @@ function Shop() {
                                         <a href="#">Sort by price: high to low</a>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="cards">
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
-                                </div>
-                                <div className="cards2">
-                                    <ShopCard />
+                                <div className="dropdown filters-dropdown">
+                                    <button className="dropdown-button">
+                                        <p className="no-hover">Filtrar Por</p>
+                                        <p>▼</p>
+                                    </button>
+                                    <div className="dropdown-content">
+                                        <Link href="#">Rustik</Link>
+                                        <Link href="#">Deco</Link>
+                                        <Link href="#">Advance</Link>
+                                        <Link href="#">Tierra</Link>
+                                        <Link href="#">Precio</Link>
+                                        <Link href="#">Color</Link>
+                                        <Link href="#">Tipo de Tela</Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <div className="cards">
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                            <ShopCard />
+                        </div>
                     </div>
                 </div>
-            </body>
+            </div>
+            <div>
+                <PageFooter />
+            </div>
         </>
     );
 }
